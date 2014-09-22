315dab5eb3dcb08c91059eff13e033bd6c01f922
son.nguyen <congsonptit@gmail.com>
2014-09-22 22:23:21 +0700

    init project

--- src/main/java/com/marinabay/cruise/model/User.java
+++ src/main/java/com/marinabay/cruise/model/User.java
@@ -1,5 +1,6 @@
 package com.marinabay.cruise.model;
 
+import com.marinabay.cruise.constant.ROLE;
 /**
  * User: son.nguyen
  * Date: 9/21/14
@@ -7,4 +8,81 @@ package com.marinabay.cruise.model;
  */
 public class User {
 
+    private Long id;
+    private String userName;
+    private String email;
+    private String password;
+    private ROLE role;
+    private String mobile;
+    private String taxiLicense;
+    private String address;
+
+    public User() {
+    }
+
+    public ROLE getRole() {
+        return role;
+    }
+
+    public void setRole(ROLE role) {
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getTaxiLicense() {
+        return taxiLicense;
+    }
+
+    public void setTaxiLicense(String taxiLicense) {
+        this.taxiLicense = taxiLicense;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
